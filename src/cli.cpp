14578d275650cf84a8b2f5dbf4b211d82d615694
ml3m <vladutifju6@gmail.com>
2024-05-02 16:23:24 +0300

    added: cli printing files

---
+++ src/cli.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void print_months(){
+    std::cout<<"1.  January  "<<std::endl;
+    std::cout<<"2.  February "<<std::endl;
+    std::cout<<"3.  March    "<<std::endl;
+    std::cout<<"4.  April    "<<std::endl;
+    std::cout<<"5.  May      "<<std::endl;
+    std::cout<<"6.  June     "<<std::endl;
+    std::cout<<"7.  July     "<<std::endl;
+    std::cout<<"8.  August   "<<std::endl;
+    std::cout<<"9.  September"<<std::endl;
+    std::cout<<"10. October  "<<std::endl;
+    std::cout<<"11. November "<<std::endl;
+    std::cout<<"12. December "<<std::endl;
+}
