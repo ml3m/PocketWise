a42bc28d6574d009eb05deaee557dc7a84b68616
mlem <159339398+ml3m@users.noreply.github.com>
2024-06-05 13:08:40 +0300

    fixed end

--- mainbuild-test/include/global.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-#include <unordered_map>
-#include <string>
-
-extern std::unordered_map<std::string, std::string> users;
-
-#endif 
--- src/UserAuthentication.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "sha256.h"
-
-class UserAuthentication{
-    public:
-        void login();
-        void createUser();
-        bool authenticateUser(const std::string& username, const std::string& password, sha256& algorithm);
-};
